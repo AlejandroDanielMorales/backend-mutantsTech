6840d25fdc1b65a127358f01143c4b44fb4fadfc
AlejandroDanielMorales <116897330+AlejandroDanielMorales@users.noreply.github.com>
2025-05-15 13:46:44 +0000

    revert

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const productsController = require("../controllers/products.controller");
 const router = express.Router();
 const uploadMiddleware = require("../middlewares/uploadFile");
-const uploadMiddlewareWebp = require("../middlewares/uploadFileWebp")
 
 router.get("/products/search", productsController.getProductsByQuery);
 
@@ -10,9 +9,9 @@ router.get("/products", productsController.getAllProducts);
 
 router.get("/products/:id", productsController.getProductById);
 
-router.post("/products", [uploadMiddlewareWebp], productsController.createProduct);
+router.post("/products", [uploadMiddleware], productsController.createProduct);
 
-router.put("/products/:id", [uploadMiddlewareWebp], productsController.updateProduct);
+router.put("/products/:id", [uploadMiddleware], productsController.updateProduct);
 
 router.delete("/products/:id", productsController.deleteProduct);
 
